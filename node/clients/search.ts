76c6cdd1a6737a3aa2db5d496233b00eb221aec1
gustavo-vasconcellos <gustavo.vasconcellos@acct.global>
2020-07-24 18:46:19 -0300

    test

--- node/clients/search.ts
+++ node/clients/search.ts
@@ -65,7 +65,7 @@ export default class Search extends ExternalClient {
   }
 
   public popular(): Promise<any> {
-    return this.get(this.routes.popular, { metric: 'chaordic-popular' })
+    return this.get(this.routes.popular, { metric: 'chaordic-popularq' })
   }
 
   private get routes() {
